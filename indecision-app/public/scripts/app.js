256a8018ef0e08e7369b6d797d0fc8204fbecc9f
Malga90 <savemeebarry@gmail.com>
2019-05-25 11:37:39 +0200

    JSX Expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,10 +2,59 @@
 
 // JSX JavaScript XML
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision app"
+  React.createElement(
+    "h1",
+    null,
+    "Indecision app"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item three"
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Malga Delegiewicz"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 28"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Gda\u0144sk"
+  )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,24 @@
 // JSX JavaScript XML
-var template = <h1>Indecision app</h1>;
+var template = (
+  <div>
+    <h1>Indecision app</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+      <li>Item three</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Malga Delegiewicz</h1>
+    <p>Age: 28</p>
+    <p>Location: Gdańsk</p>
+  </div>
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
