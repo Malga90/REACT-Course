060517ce6e247dc8284eedf06d07a3af7549e391
Malga90 <savemeebarry@gmail.com>
2019-05-27 17:24:30 +0200

    events and sttributes

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,46 +1,91 @@
 "use strict";
 
-// arguments object - no longer bound with arrow functions
+// JSX JavaScript XML
 
-var add = function add(a, b) {
-  //   console.log(arguments);
-  return a + b;
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
+};
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item 2016"
+    )
+  )
+);
+
+var count = 0;
+var addOne = function addOne() {
+  console.log("addOne");
 };
 
-console.log(add(55, 1, 2016));
-
-// this keyword - no longer bound
-
-var user = {
-  name: "Malga",
-  cities: ["Gdańsk", "Poznań", "London"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + " has lived in " + city;
-    });
-
-    // this.cities.forEach(city => {
-    //   console.log(this.name + " " + " has lived in " + city);
-    // });
-  }
+var minusOne = function minusOne() {
+  console.log("minusOne");
 };
 
-console.log(user.printPlacesLived());
-
-// Challenge
-
-var multiplier = {
-  numbers: [1, 2, 3],
-  multiplyBy: 6,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var reset = function reset() {
+  console.log("reset");
 };
 
-console.log(multiplier.multiply());
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Count: ",
+    count
+  ),
+  React.createElement(
+    "button",
+    { onClick: addOne },
+    "+1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: minusOne },
+    "-1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: reset },
+    "Reset"
+  )
+);
+
+console.log(templateTwo);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(templateTwo, appRoot);
