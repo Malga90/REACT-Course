c5cae7bb7b79733eb109abc1064ad282cc1aa6b8
Malga90 <savemeebarry@gmail.com>
2019-06-01 13:44:17 +0200

    adding state to visibility toggle

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,28 +1,62 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState(() => {
+      return {
+        visibility: !this.state.visibility
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? "Hide details" : "Show details"}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>There are some details!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
-const visibilityToggle = () => {
-  visibility = !visibility;
-  renderApp();
-};
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
 
-const appRoot = document.getElementById("app");
+// let visibility = false;
 
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={visibilityToggle}>
-        {visibility ? "Hide details" : "Show details"}
-      </button>
-      {visibility && (
-        <div>
-          <p>There are some details!</p>
-        </div>
-      )}
-    </div>
-  );
+// const visibilityToggle = () => {
+//   visibility = !visibility;
+//   renderApp();
+// };
 
-  ReactDOM.render(template, appRoot);
-};
+// const appRoot = document.getElementById("app");
 
-renderApp();
+// const renderApp = () => {
+//   const template = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={visibilityToggle}>
+//         {visibility ? "Hide details" : "Show details"}
+//       </button>
+//       {visibility && (
+//         <div>
+//           <p>There are some details!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+
+//   ReactDOM.render(template, appRoot);
+// };
+
+// renderApp();
