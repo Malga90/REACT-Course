ada8a0f1d671d0f94a33fec7808ea78905c9f09b
Malga90 <savemeebarry@gmail.com>
2019-07-12 21:44:22 +0200

    default prop values

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,9 +5,10 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: 0
+      count: props.count
     };
   }
+
   handleAddOne() {
     this.setState(prevState => {
       return {
@@ -29,6 +30,7 @@ class Counter extends React.Component {
       };
     });
   }
+
   render() {
     return (
       <div>
@@ -40,6 +42,10 @@ class Counter extends React.Component {
     );
   }
 }
+
+Counter.defaultProps = {
+  count: 0
+};
 
 ReactDOM.render(<Counter />, document.getElementById("app"));
 
