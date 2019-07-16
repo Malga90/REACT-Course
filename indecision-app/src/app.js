d6a2e3a34696f727128be7cfbf77486711d2e149
Malga90 <savemeebarry@gmail.com>
2019-07-16 19:39:50 +0200

    es6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,11 @@
-console.log("app.js is running!!!");
+// import "./utils.js";
+// import { square, add } from "./utils.js";
+
+// console.log("app.js is running!!!");
+// console.log(square(4));
+// console.log(add(4, 216));
+
+console.log(isAdult(18));
+console.log(canDrink(15));
+
+import { isAdult, canDrink } from "./person.js";
