cfb9ba5f4098628b1eb3ea4aece1d6d9541b87a8
Malga90 <savemeebarry@gmail.com>
2019-07-22 23:15:39 +0200

    setting up 404

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -12,14 +12,17 @@ const EditExpensePage = () => <div>Edit expense, nooooow</div>;
 
 const HelpPage = () => <div>Somebody help!</div>;
 
+const NotFoundPage = () => <div>404!</div>;
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
