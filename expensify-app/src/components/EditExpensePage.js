0da38ba77462d35704dc8c6103a261f74a82e58e
Malga90 <savemeebarry@gmail.com>
2019-07-27 12:31:38 +0200

    setting up redux

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const EditExpensePage = () => <div>Edit expense, nooooow</div>;
+const EditExpensePage = props => {
+  console.log(props);
+  return <div>Edit expense with {props.match.params.id}</div>;
+};
 
 export default EditExpensePage;
