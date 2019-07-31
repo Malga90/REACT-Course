ca9c309c39e1cbf2bc71da17b56abb6c69008df2
Malga90 <savemeebarry@gmail.com>
2019-07-31 09:24:33 +0200

    es6 - array descructuring

--- expensify-app/src/playground/descructuring.js
+++ expensify-app/src/playground/descructuring.js
@@ -1,8 +1,10 @@
+// Object desctucturing
+
 const person = {
   name: "Małga",
   age: 28,
   location: {
-    city: "London",
+    //city: "London",
     temp: 22
   }
 };
@@ -11,12 +13,12 @@ const { name: firstName = "JAne Doe", age } = person;
 // const name = person.name;
 // const age = person.age;
 
-console.log(`${firstName} is ${age}.`);
+// console.log(`${firstName} is ${age}.`);
 
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-  console.log(`It's ${temperature} in ${city}`);
-}
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}`);
+// }
 
 const book = {
   name: "Harry Potter",
@@ -31,3 +33,14 @@ const { name: publisherName = "Self-published" } = book.publisher;
 if (publisherName) {
   console.log(publisherName);
 }
+
+// Array descucturing
+
+const address = ["1299 Jupiter Street", "Philapelphia", "Pennsylvnia", "19478"];
+const [, city, state = "NY"] = address;
+console.log(`You are in ${city} ${state}.`);
+
+const item = ["Coffee", "$2.20", "$2.50", "$2.75"];
+
+const [selledItem = "Cheese", , medium] = item;
+console.log(`A medium ${selledItem} costs ${medium}`);
