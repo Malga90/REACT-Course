25179334cc581e808f3552090e83f6efba980ed9
Malga90 <savemeebarry@gmail.com>
2019-08-23 23:07:31 +0200

    actions dispatched

--- expensify-app/src/playground/redux_101.js
+++ expensify-app/src/playground/redux_101.js
@@ -1,9 +1,46 @@
 import { createStore } from "redux";
 
-console.log("redux, yeah");
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+console.log(store.getState());
+
+// Actions - object that gets sent to the store
+
+// increment
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+//reset
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
 });
 
 console.log(store.getState());
