564ae30aa343c2e41a4f5bac81c653b7e9495d07
Malga90 <savemeebarry@gmail.com>
2019-08-24 13:45:38 +0200

    descructuring arrays in es6

--- expensify-app/src/playground/descructuring.js
+++ expensify-app/src/playground/descructuring.js
@@ -1,10 +1,11 @@
 console.log("nanannanan");
 
+//// OBJECT DESCRTUCTURING
 const person = {
   name: "Malga",
   age: 30,
   location: {
-    city: "London",
+    // city: "London",
     temp: 81
   }
 };
@@ -15,10 +16,10 @@ const { name: firstName = "Jane", age } = person;
 
 console.log(`${firstName} is ${age}`);
 
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-  console.log(`It is ${temperature} in ${city}`);
-}
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//   console.log(`It is ${temperature} in ${city}`);
+// }
 
 const book = {
   title: "Ego is the enemy",
@@ -31,3 +32,15 @@ const book = {
 const { name: publisherName = "Self- Published" } = book.publisher;
 
 console.log(publisherName);
+
+/// ARRAY DESCRUCTURING
+
+const address = ["1299 South Juniper Street", "NYC", "New York", "19147"];
+
+const [, city, state = "Pensylvania"] = address;
+
+console.log(`You are in ${city}, ${state}`);
+
+const item = ["Coffee", "$1.8", "$2.1", "$2.5"];
+const [menuItem, , medium] = item;
+console.log(`A medium ${menuItem} costs ${medium}.`);
